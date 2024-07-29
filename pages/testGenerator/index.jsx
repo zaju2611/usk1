abc4d0e84132840fc66c1c538c9a1304a8821b25
Jakub Zając <jakub.zajac2611@gmail.com>
2024-07-29 10:56:09 +0200

    change url to fetch data

--- pages/testGenerator/index.jsx
+++ pages/testGenerator/index.jsx
@@ -69,7 +69,7 @@ export default function TestGenerator({ tests }) {
 }
 
 export async function getServerSideProps() {
-	const res = await fetch("http://localhost:3000/api/tests");
+	const res = await fetch("https://usk1.vercel.app//api/tests");
 	const data = await res.json();
 
 	return {
