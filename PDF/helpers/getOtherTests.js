575d7be63f9d1d12db313f6736e3553e95d42d78
jzajac <72804311+zaju2611@users.noreply.github.com>
2025-06-13 07:34:19 +0200

    Update getOtherTests.js

--- PDF/helpers/getOtherTests.js
+++ PDF/helpers/getOtherTests.js
@@ -5,7 +5,7 @@ export default function getOtherTests(
 ) {
 	return allTests.filter(
 		(test) =>
-			!referralTests.some((refTest) => refTest.value === test.value) && !hasSampleCollection
-			// (!hasSampleCollection || test.value !== "Pobranie materiału")
+			!referralTests.some((refTest) => refTest.value === test.value) && 
+			(!hasSampleCollection || test.value !== "Pobranie materiału")
 	);
 }
